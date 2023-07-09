b9b7dd21cbabbdae558172031766e06e336e800d
Daniel Ivanov <danielskiapple@gmail.com>
2023-07-09 14:54:29 +0300

    Add task for Inheritance- Exercise

---
+++ Inheritance/Zoo/Main.java
@@ -0,0 +1,9 @@
+package Inheritance_Ex.x02_Zoo;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
