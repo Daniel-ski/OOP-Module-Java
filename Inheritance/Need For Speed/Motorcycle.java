5282b8ee465af5429fa547d7f3192f2d9b305e3f
Daniel Ivanov <danielskiapple@gmail.com>
2023-07-10 22:37:18 +0300

    Add task for Inheritance- Exercise

---
+++ Inheritance/Need For Speed/Motorcycle.java
@@ -0,0 +1,9 @@
+package Inheritance_Ex.x04_NeedForSpeed;
+
+public class Motorcycle extends Vehicle{
+
+    public Motorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+
+    }
+}
