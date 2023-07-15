4e8b5974f8803e485353982bec103a489ed756b6
Daniel Ivanov <danielskiapple@gmail.com>
2023-07-15 22:43:05 +0300

    Add task for Inheritance- Exercise

---
+++ Inheritance/Restaurant/HotBeverage.java
@@ -0,0 +1,10 @@
+package Inheritance_Ex.x05_Restaurant;
+
+import java.math.BigDecimal;
+
+public class HotBeverage extends Beverages{
+
+    public HotBeverage(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
+}
