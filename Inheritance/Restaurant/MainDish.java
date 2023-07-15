0ebe127c398bd89edb9197396a4407f85a1c9ed2
Daniel Ivanov <danielskiapple@gmail.com>
2023-07-15 22:43:39 +0300

    Add task for Inheritance- Exercise

---
+++ Inheritance/Restaurant/MainDish.java
@@ -0,0 +1,10 @@
+package Inheritance_Ex.x05_Restaurant;
+
+import java.math.BigDecimal;
+
+public class MainDish extends Food{
+
+    public MainDish(String name, BigDecimal price, double grams) {
+        super(name, price, grams);
+    }
+}
