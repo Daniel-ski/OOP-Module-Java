97e417a1ff2c4f89e403d9e89489b31eaffb2cc6
Daniel Ivanov <danielskiapple@gmail.com>
2023-07-17 20:23:55 +0300

    Add task for Inheritance- Exercise

---
+++ Inheritance/Restaurant/Starter.java
@@ -0,0 +1,10 @@
+package Inheritance_Ex.x05_Restaurant;
+
+import java.math.BigDecimal;
+
+public class Starter extends Food{
+
+    public Starter(String name, BigDecimal price, double grams) {
+        super(name, price, grams);
+    }
+}
