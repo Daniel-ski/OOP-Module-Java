b31e973707e713d87529d7b0764bef5bd7e1d637
Daniel Ivanov <danielskiapple@gmail.com>
2023-08-09 23:12:35 +0300

    Add task for Interface and Abstract Classes- Exercise

---
+++ Interfaces And Abstract Classes/Food Shortage/Rebel.java
@@ -0,0 +1,37 @@
+package InterfacesAndAbstractClasses_Ex.x04_FoodShortage;
+
+public class Rebel implements Person,Buyer {
+
+    private static final int INITIAL_AMOUNT_FOOD = 0;
+    private String name;
+    private int age;
+    private String group;
+    private int food;
+
+    public Rebel(String name, int age, String group) {
+        this.name = name;
+        this.age = age;
+        this.group = group;
+        this.food = INITIAL_AMOUNT_FOOD;
+    }
+
+    @Override
+    public void buyFood() {
+        this.food += 5;
+    }
+
+    @Override
+    public int getFood() {
+        return this.food;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getAge() {
+        return this.age;
+    }
+}
